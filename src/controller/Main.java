90c144af67758cda711763fab56db686c0eaa6d8
kundan kumar <kd.kundan9572@gmail.com>
2020-09-11 12:18:09 +0530

    Initial Commit

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,23 @@
+package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main{
+	Skill skill = new Skill();
+	SkillDAO skilldao = new SkillDAO();
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("List of all Skills are :");
+		
+		
+		
+		
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,40 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO {
+	List<Skill>  list = new ArrayList<Skill>();
+
+	
+	public List<Skill >listAllSkills() throws ClassNotFoundException, SQLException, IOException{
+		
+		Skill skill = null;
+		ConnectionManager con = new ConnectionManager();
+		
+		Statement st= con.getConnection().createStatement();
+		
+		String sql= "SELECT * FROM SKILLS";
+		ResultSet rs= st.executeQuery(sql);
+		
+		while(rs.next()) {
+			skill= new Skill();
+			
+			skill.setSkillId("SKILLID");
+			skill.setSkillName(rs.getString("NAME"));
+			list.add(skill);
+		
+		}
+		
+		return list;
+		
+	}
+}
